cf2f895ac16c552fd2d0483afa959eae90db90c8
Caelan Roberge-Toll <caelanrt15@gmail.com>
2025-11-08 21:29:04 -0500

    doing some more robust error handling to make sure the program doesn't crash

--- hello.py
+++ hello.py
@@ -7,6 +7,20 @@ my_name = input('>')
 print('It is good to meet you, ' + my_name)
 print('The length of your name is:')
 print(len(my_name))
-print('What is your age?')
-my_age = int(input('>'))
-print('You will be ' + str(my_age + 1) + ' in a year.')
+
+flag = 1
+
+while flag  == 1:
+    print('What is your age?')
+
+    my_age_string = input('>')
+
+    if (my_age_string.isdigit() == True):
+        my_age = int(my_age_string)
+        if my_age <= 0:
+            print('Invalid age - enter an age above 0')
+        else:
+            print('You will be ' + str(my_age + 1) + ' in a year.')
+            flag = 0
+    else:
+        print('Invalid age - enter a number')
