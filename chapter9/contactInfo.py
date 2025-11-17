d6dd5b9172004593a07f91b5f16406ed441159bb
CaelanRT <caelanrt15@gmail.com>
2025-11-17 16:20:43 -0500

    well i wasn't able to complete the project by myself becasue honestly those are hard to do tbh, he sets them up in a way that i don't know how he wants it, but i went through it

--- chapter9/contactInfo.py
+++ chapter9/contactInfo.py
@@ -1,14 +1,38 @@
 import pyperclip, re
 
-text = pyperclip.paste()
+text = str(pyperclip.paste())
 phone_pattern = re.compile(r'''(
     (\d{3}|\(\d{3}\))?  # Area code
     (\s|-|\.)?  # Separator
-    \d{3}  # First three digits
+    (\d{3})  # First three digits
     (\s|-|\.)  # Separator
-    \d{4}  # Last four digits
-    (\s*(ext|x|ext\.)\s*\d{2,5})?  # Extension
+    (\d{4})  # Last four digits
+    (\s*(ext|x|ext\.)\s*(\d{2,5}))?  # Extension
     )''', re.VERBOSE)
 
-print(phone_pattern.findall(text))
+email_pattern = re.compile(r'''(
+                        [a-zA-Z0-9.%+-]+    # name
+                        @    # at sign 
+                        [a-zA-Z0-9.-]+    # domain name
+                        (\.[a-zA-Z]{2,4})    # period
+                        
+                          )''', re.VERBOSE)
+
+matches = []
+
+for group in phone_pattern.findall(text):
+    phone_number = '-'.join([group[1], group[3], group[5]])
+    if group[6] != '':
+        phone_number += ' x' + group[6]
+    matches.append(phone_number)
+
+for groups in email_pattern.findall(text):
+    matches.append(groups[0])
+
+if len(matches) > 0:
+    pyperclip.copy('\n'.join(matches))
+    print('Copied to clipboard:')
+    print('\n'.join(matches))
+else:
+    print('No phone numbers or emails found.')
 
